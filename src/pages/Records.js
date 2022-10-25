6dfe1d6617887aa35ecdf69f2ed0fe6d0b2136dd
bakjonghun <bakjonghun@hikick.kr>
2022-10-25 18:24:33 +0900

    add: show ride info when user click ride name

--- src/pages/Records.js
+++ src/pages/Records.js
@@ -1,23 +1,32 @@
+import { Button, Drawer } from 'antd';
+import React, { useState } from 'react';
+
 import { BackofficeTable } from '../components';
-import { Button } from 'antd';
-import { Link } from 'react-router-dom';
-import React from 'react';
 import { RecordStatus } from '../components/Record/RecordStatus';
-import { UserAddOutlined } from '@ant-design/icons';
+import { RideInfo } from '../components/Ride/RideInfo';
 import { UserInfoProvider } from '../components/User/UserInfoProvider';
 import dayjs from 'dayjs';
 import { getClient } from '../tools';
 
 export const Records = () => {
+  const [selectedRideId, setSelectedRideId] = useState();
+  const onClickName = (record) => () =>
+    setSelectedRideId(record.properties?.coreservice?.rideId);
+  console.log(selectedRideId);
   const columns = [
     {
       title: '상태',
-      dataIndex: 'name',
-      render: (name, record) => <RecordStatus record={record} />,
+      dataIndex: 'recordId',
+      render: (recordId, record) => <RecordStatus record={record} />,
     },
     {
       title: '이름',
       dataIndex: 'name',
+      render: (name, record) => (
+        <Button type="text" onClick={onClickName(record)}>
+          {name}
+        </Button>
+      ),
     },
     {
       title: '결제금액(초기 금액)',
@@ -60,21 +69,26 @@ export const Records = () => {
     getClient('coreservice-payments').then((c) => c.get('/records', opts));
 
   return (
-    <BackofficeTable
-      title="결제내역 목록"
-      hasSearch={true}
-      columns={columns}
-      onRequest={onRequest}
-      scroll={{ x: 1200 }}
-      dataSourceKey="records"
-      rowKey="recordId"
-      buttons={
-        <Link to="/records/add">
-          <Button icon={<UserAddOutlined />} type="primary">
-            라이드 시작
-          </Button>
-        </Link>
-      }
-    />
+    <>
+      <BackofficeTable
+        title="결제내역 목록"
+        hasSearch={true}
+        columns={columns}
+        onRequest={onRequest}
+        scroll={{ x: 1200 }}
+        dataSourceKey="records"
+        rowKey="recordId"
+      />
+      <Drawer
+        closable
+        title="라이드 상세조회"
+        placement="right"
+        onClose={() => setSelectedRideId()}
+        visible={selectedRideId}
+        width={'80%'}
+      >
+        <RideInfo rideId={selectedRideId} />
+      </Drawer>
+    </>
   );
 };
